fa31a8a79eb9adecc2d4a05db9699c8d04ba463c
Paulo 10 <80061097+Paulooo10@users.noreply.github.com>
2022-09-20 15:56:27 +0200

    Add setup ROTARY_ANGLE_SENSOR

---
+++ asset/test étape 2.ino
@@ -0,0 +1,50 @@
+// C++ code
+//
+/*
+  Keyboard
+  Plays a pitch that changes based on a changing
+  input circuit:
+  * 5 pushbuttons from +5V to analog in 0 through
+  3
+  * 5 10K resistors from analog in 0 through 3 to
+  ground
+  * 8-ohm speaker on digital pin 8
+*/
+
+
+int pos = 0;
+
+void setup()
+{
+  pinMode(A0, INPUT);
+  pinMode(8, OUTPUT);
+  pinMode(A1, INPUT);
+  pinMode(A2, INPUT);
+  pinMode(A3, INPUT);
+  pinMode(A4, INPUT);
+  pinMode(ROTARY_ANGLE_SENSOR, INPUT);
+  pinMode(LED,OUTPUT); 
+}
+
+void loop()
+{
+  // if button press on A0 is detected
+  if (digitalRead(A0) == HIGH) {
+    tone(8, 261.6, 100); // play tone 57 (A4 = 440 Hz)
+  }
+  // if button press on A1 is detected
+  if (digitalRead(A1) == HIGH) {
+    tone(8, 293.7, 100); // play tone 59 (B4 = 494 Hz)
+  }
+  // if button press on A0 is detected
+  if (digitalRead(A2) == HIGH) {
+    tone(8, 329.6, 100); // play tone 60 (C5 = 523 Hz)
+  }
+  if (digitalRead(A3) == HIGH) {
+    tone(8, 392.0, 100); // play tone 62 (D5 = 587 Hz)
+  }
+  if (digitalRead(A4) == HIGH) {
+    tone(8, 440, 100); // play tone 63 (D#5 = 622 Hz)
+  }
+  delay(10); // Delay a little bit to improve simulation performance
+}
