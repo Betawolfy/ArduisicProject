18420cca8f73ab60108abea11fb738cba3db0109
Paulo 10 <80061097+Paulooo10@users.noreply.github.com>
2022-09-20 14:12:00 +0200

    Good frequences
    CHARLOTTE IS MINE

--- main.ino
+++ main.ino
@@ -2,7 +2,6 @@
 //
 /*
   Keyboard
-
   Plays a pitch that changes based on a changing
   input circuit:
   * 3 pushbuttons from +5V to analog in 0 through
@@ -28,21 +27,21 @@ void loop()
 {
   // if button press on A0 is detected
   if (digitalRead(A0) == HIGH) {
-    tone(8, 440, 100); // play tone 57 (A4 = 440 Hz)
+    tone(8, 261.6, 100); // play tone 57 (A4 = 440 Hz)
   }
   // if button press on A1 is detected
   if (digitalRead(A1) == HIGH) {
-    tone(8, 494, 100); // play tone 59 (B4 = 494 Hz)
+    tone(8, 293.7, 100); // play tone 59 (B4 = 494 Hz)
   }
   // if button press on A0 is detected
   if (digitalRead(A2) == HIGH) {
-    tone(8, 523, 100); // play tone 60 (C5 = 523 Hz)
+    tone(8, 329.6, 100); // play tone 60 (C5 = 523 Hz)
   }
   if (digitalRead(A3) == HIGH) {
-    tone(8, 587, 100); // play tone 62 (D5 = 587 Hz)
+    tone(8, 392.0, 100); // play tone 62 (D5 = 587 Hz)
   }
   if (digitalRead(A4) == HIGH) {
-    tone(8, 622, 100); // play tone 63 (D#5 = 622 Hz)
+    tone(8, 440, 100); // play tone 63 (D#5 = 622 Hz)
   }
   delay(10); // Delay a little bit to improve simulation performance
 }
