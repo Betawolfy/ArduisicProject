6103d616ebf7c0bebdcb468585c0dba6d40eb416
Paulo 10 <80061097+Paulooo10@users.noreply.github.com>
2022-10-13 08:42:02 +0200

    Update test étape 2.ino
    octave 4 update

--- asset/test étape 2.ino
+++ asset/test étape 2.ino
@@ -30,21 +30,21 @@ void loop()
 {
   // if button press on A0 is detected
   if (digitalRead(A0) == HIGH) {
-    tone(8, 261.6, 100); // play tone 57 (A4 = 440 Hz)
+    tone(8, 523.3, 100); // play tone 57 (A4 = 440 Hz)
   }
   // if button press on A1 is detected
   if (digitalRead(A1) == HIGH) {
-    tone(8, 293.7, 100); // play tone 59 (B4 = 494 Hz)
+    tone(8, 587.3, 100); // play tone 59 (B4 = 494 Hz)
   }
   // if button press on A0 is detected
   if (digitalRead(A2) == HIGH) {
-    tone(8, 329.6, 100); // play tone 60 (C5 = 523 Hz)
+    tone(8, 659.3, 100); // play tone 60 (C5 = 523 Hz)
   }
   if (digitalRead(A3) == HIGH) {
-    tone(8, 392.0, 100); // play tone 62 (D5 = 587 Hz)
+    tone(8, 784.0, 100); // play tone 62 (D5 = 587 Hz)
   }
   if (digitalRead(A4) == HIGH) {
-    tone(8, 440, 100); // play tone 63 (D#5 = 622 Hz)
+    tone(8, 880.0, 100); // play tone 63 (D#5 = 622 Hz)
   }
   delay(10); // Delay a little bit to improve simulation performance
 }
