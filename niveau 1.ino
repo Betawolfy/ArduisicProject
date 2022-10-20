b1c7bfeb4577983e522ba4a9ea35fd5e50fab42e
Vincent MARAIS <80059002+Betawolfy@users.noreply.github.com>
2022-10-20 08:17:00 +0200

    Update niveau 1.ino

--- niveau 1.ino
+++ niveau 1.ino
@@ -1,19 +1,18 @@
-// C++ code
-//
-/*
-  Keyboard
-  Plays a pitch that changes based on a changing
-  input circuit:
-  * 5 pushbuttons from +5V to analog in 0 through
-  3
-  * 5 10K resistors from analog in 0 through 3 to
-  ground
-  * 8-ohm speaker on digital pin 8
-*/
+/* MISSION:
+ *  - Jouer un son avec le Grove - buzzer quand on appuie sur un bouton pressoir.
+ *  - Pouvoir Jouer plusieurs note (octave) avec plusieurs boutons pressoir. 
+ *  --------------------------------
+ *  Vincent Marais Paul Rallet
+ *  github: https://github.com/Betawolfy
+ *  --------------------------------
+ *  NOTES TECHNIQUES: 
+ *  le Grove - Buzzer doit être connecté sur le port D8.
+ *  Les boutons pressoirs qui jouent des notes doivent être connectés sur A0, A1, A2, A3 et A4.
+ */
 
 int pos = 0;
 
-void setup()
+void setup() // Procédure d'initialisation.
 {
   pinMode(A0, INPUT);
   pinMode(8, OUTPUT);
@@ -23,25 +22,27 @@ void setup()
   pinMode(A4, INPUT);
 }
 
-void loop()
+void loop() // boucle infinie
 {
-  // if button press on A0 is detected
+  // Si le bouton A0 est poussé
   if (digitalRead(A0) == HIGH) {
-    tone(8, 65.41, 100); // play tone 57 (A4 = 440 Hz)
+    tone(8, 65.41, 100); // jouer son 57 (A4 = 440 Hz)
   }
-  // if button press on A1 is detected
+  // Si le bouton A1 est poussé
   if (digitalRead(A1) == HIGH) {
-    tone(8, 73.42, 100); // play tone 59 (B4 = 494 Hz)
+    tone(8, 73.42, 100); // jouer son 59 (B4 = 494 Hz)
   }
-  // if button press on A0 is detected
+  // Si le bouton A2 est poussé
   if (digitalRead(A2) == HIGH) {
-    tone(8, 82.41, 100); // play tone 60 (C5 = 523 Hz)
+    tone(8, 82.41, 100); // jouer son 60 (C5 = 523 Hz)
   }
+  // Si le bouton A3 est poussé
   if (digitalRead(A3) == HIGH) {
-    tone(8, 98, 100); // play tone 62 (D5 = 587 Hz)
+    tone(8, 98, 100); // jouer son 62 (D5 = 587 Hz)
   }
+  // Si le bouton A4 est poussé
   if (digitalRead(A4) == HIGH) {
-    tone(8, 110, 100); // play tone 63 (D#5 = 622 Hz)
+    tone(8, 110, 100); // jouer son 63 (D#5 = 622 Hz)
   }
   delay(10); // Delay a little bit to improve simulation performance
 }
