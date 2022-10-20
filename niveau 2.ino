b3c429f3888a424174f65e19794fcdac5c2a418e
Vincent MARAIS <80059002+Betawolfy@users.noreply.github.com>
2022-10-20 08:32:37 +0200

    Update niveau 2.ino

--- niveau 2.ino
+++ niveau 2.ino
@@ -52,46 +52,55 @@ void loop() // Boucle infini
   Serial.println(EtatBoutonActuel);
   // programmation de la désactivation du bouton
   if (Compteur % 2 == 0) {
-    // on vérifie le reste de la division
+  // si le reste de la division est égal à 0
+    
+    // Si le bouton A0 est poussé
   if (digitalRead(A0) == HIGH) {
-    tone(8, 261.6, 100); // play tone 57 (A4 = 440 Hz)
-    Serial.println("Arduisic - playing tone 57");
+    tone(8, 261.6, 100); // Jouer le son
+    Serial.println("Arduisic - playing tone 57"); 
+    // On met le son joué dans le moniteur série
+    
   }
-  // if button press on A1 is detected
+    // Si le bouton A1 est poussé
   if (digitalRead(A1) == HIGH) {
-    tone(8, 293.7, 100); // play tone 59 (B4 = 494 Hz)
+    tone(8, 293.7, 100); 
     Serial.println("Arduisic - playing tone 59");
   }
-  // if button press on A0 is detected
+    // Si le bouton A2 est poussé
   if (digitalRead(A2) == HIGH) {
-    tone(8, 329.6, 100); // play tone 60 (C5 = 523 Hz)
+    tone(8, 329.6, 100); 
     Serial.println("Arduisic - playing tone 60");
   }
+   // Si le bouton A3 est poussé
   if (digitalRead(A3) == HIGH) {
-    tone(8, 392.0, 100); // play tone 62 (D5 = 587 Hz)
+    tone(8, 392.0, 100); 
     Serial.println("Arduisic - playing tone 62");
   }
+   // Si le bouton A4 est poussé
   if (digitalRead(A4) == HIGH) {
-    tone(8, 440, 100); // play tone 63 (D#5 = 622 Hz)
+    tone(8, 440, 100); 
     Serial.println("Arduisic - playing tone 63");
   }
   } else {
-    // si l'on appuie pas sur le bouton la lampe reste
-    // allumée.
+    // sinon (le reste est différent de 0)
+    
+    // Si le bouton A0 est poussé 
   if (digitalRead(A0) == HIGH) {
     tone(8, 523.3, 100); // play tone 57 (A4 = 440 Hz)
   }
-  // if button press on A1 is detected
+    // Si le bouton A1 est poussé
   if (digitalRead(A1) == HIGH) {
     tone(8, 587.3, 100); // play tone 59 (B4 = 494 Hz)
   }
-  // if button press on A0 is detected
+    // Si le bouton A2 est poussé
   if (digitalRead(A2) == HIGH) {
     tone(8, 659.3, 100); // play tone 60 (C5 = 523 Hz)
   }
+   // Si le bouton A3 est poussé
   if (digitalRead(A3) == HIGH) {
     tone(8, 784.0, 100); // play tone 62 (D5 = 587 Hz)
   }
+   // Si le bouton A4 est poussé
   if (digitalRead(A4) == HIGH) {
     tone(8, 880.0, 100); // play tone 63 (D#5 = 622 Hz)
   };
